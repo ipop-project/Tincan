1d64876c8ab6a4415433f366507f78cc54355322
Vahid Daneshmand <v.daneshmand@gmail.com>
2018-02-18 23:27:46 -0500

    Update config.mk

--- trunk/build/config.mk
+++ trunk/build/config.mk
@@ -1,11 +1,7 @@
-#To build Raspberry Pi set ARCH=arm7 and PLAT=rpi
-#To build CentOS set PLAT=centos
-
 CC = g++
 
-ARCH = x64
+ARCH = $(shell uname -m)
 OPT = release
-PLAT = ubuntu
 
 INC_DIR = ../include
 INC_DIR_LNX = $(INC_DIR)/linux
@@ -14,7 +10,7 @@ EXT_INC_DIR = ../../external/include
 SRC_DIR = ../src
 SRC_DIR_LNX = $(SRC_DIR)/linux
 
-EXT_LIB_DIR = ../../external/lib/$(OPT)/$(ARCH)/$(PLAT)
+EXT_LIB_DIR = ../../external/3rd-Party-Libs/$(OPT)
 OUT = ../out
 OBJ_DIR = $(OUT)/$(OPT)/$(ARCH)/obj
 BIN_DIR = $(OUT)/$(OPT)/$(ARCH)
