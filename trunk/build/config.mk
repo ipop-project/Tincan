bbb4bcd07ad1013656c7fa5eb6a76bba3e211681
Vahid Daneshmand <v.daneshmand@gmail.com>
2018-09-11 11:14:10 -0400

    Add Debug Mode Flag to the Command Line

--- trunk/build/config.mk
+++ trunk/build/config.mk
@@ -1,7 +1,15 @@
 CC = g++
 
+DEBUG ?= 0
+ifeq ($(DEBUG), 1)
+    OPT = debug
+    cflags_cc = -O0
+else
+	OPT = release
+    cflags_cc = -O3
+endif
+
 ARCH = $(shell uname -m)
-OPT = release
 
 INC_DIR = ../include
 INC_DIR_LNX = $(INC_DIR)/linux
@@ -20,7 +28,7 @@ TARGET = $(patsubst %,$(BIN_DIR)/%,$(BINARY))
 
 defines = -DLINUX -D_IPOP_LINUX -DWEBRTC_POSIX -DWEBRTC_LINUX -D_GLIBCXX_USE_CXX11_ABI=0
 
-cflags_cc = -std=c++14 -O3 -pthread -g2 -gsplit-dwarf -fno-strict-aliasing --param=ssp-buffer-size=4 -fstack-protector -funwind-tables -fPIC -pipe -Wall -fno-rtti
+cflags_cc += -std=c++14 -pthread -g2 -gsplit-dwarf -fno-strict-aliasing --param=ssp-buffer-size=4 -fstack-protector -funwind-tables -fPIC -pipe -Wall -fno-rtti
 
 LIBS = -ljsoncpp -lrtc_p2p -lrtc_base -lrtc_base_approved -lfield_trial_default -lboringssl -lboringssl_asm -lprotobuf_lite -lpthread -lnetlink -lutil
 
